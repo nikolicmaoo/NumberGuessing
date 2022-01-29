5cc9729e13a6b54302f5ef9ee087e0d24705ebba
acabs <acabs@MAGDALENA>
2022-01-29 20:34:53 +0100

    Add project files.

---
+++ NumberGuessing/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace NumberGuessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber = new Random().Next(1, 100);
+
+             static int GetGuess()
+            {
+                Console.WriteLine("Please enter your guess.");
+                int guess = 0;
+                try
+                {
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You did not enter a valid guess.");
+                    guess = GetGuess();
+                }
+                return guess;
+            }
+            int allowedTries = 5;
+            int numberOfTries = 0;
+
+            while (true)
+            {
+                int guess = GetGuess();
+                numberOfTries++;
+                if (guess == randomNumber)
+                {
+                    Console.WriteLine("Your guess was correct!");
+                    break;
+                }
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Your guess was too high");
+                }
+                if (guess < randomNumber)
+                {
+                    Console.WriteLine("Your guess was too low");
+                }
+                if (numberOfTries == allowedTries)
+                {
+                    Console.WriteLine($"The number was: {randomNumber}");
+                    break;
+                }
+                Console.Write($"You have {allowedTries - numberOfTries} tries left. Enter another number: ");
+            }
+        }
+    }
+}
